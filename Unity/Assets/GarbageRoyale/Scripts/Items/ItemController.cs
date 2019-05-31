02e8cd81fb569d06374f310f8eae0bb3c9ecdada
Pim <pim103@hotmail.fr>
2019-05-31 10:53:01 +0200

    Fix craft item
    Master set id of item and send it

---
+++ Unity/Assets/GarbageRoyale/Scripts/Items/ItemController.cs
@@ -0,0 +1,43 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GarbageRoyale.Scripts.Items
+{
+    public class ItemController : MonoBehaviourPunCallbacks
+    {
+        [SerializeField]
+        private GameController gc;
+
+        public void brokeBottle(int id)
+        {
+            photonView.RPC("brokeBottleRpc", RpcTarget.MasterClient, id);
+        }
+
+        [PunRPC]
+        private void brokeBottleRpc(int id)
+        {
+            if(!PhotonNetwork.IsMasterClient)
+            {
+                return;
+            }
+            photonView.RPC("addSpecificBrokenBottle", RpcTarget.All, id, gc.items.Count);
+        }
+
+        [PunRPC]
+        private void addSpecificBrokenBottle(int id, int idItem)
+        {
+            GameObject bottle = gc.items[id];
+            bottle.SetActive(false);
+
+            GameObject brokenBottle = ObjectPooler.SharedInstance.GetPooledObject(4);
+            brokenBottle.transform.position = bottle.transform.position;
+            brokenBottle.transform.rotation = bottle.transform.rotation;
+            brokenBottle.SetActive(true);
+            brokenBottle.GetComponent<Item>().id = idItem;
+
+            gc.items.Add(idItem, brokenBottle);
+        }
+    }
+}
