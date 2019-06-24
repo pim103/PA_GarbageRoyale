d73b2b7a4e0dbb61873232455fff155d35d1b0e3
Pim <pim103@hotmail.fr>
2019-06-24 14:42:26 +0200

    Add cam spectate

---
+++ Unity/Assets/GarbageRoyale/Scripts/PlayerController/SpectateMode.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GarbageRoyale.Scripts.PlayerController
+{
+    public class SpectateMode : MonoBehaviour
+    {
+        [SerializeField]
+        private GameController gc;
+
+        [HideInInspector]
+        public int idCamSpectate;
+
+        public void SwitchCam(int id)
+        {
+            for (var i = 0; i < PhotonNetwork.CurrentRoom.PlayerCount; i++)
+            {
+                if (i != id && !gc.players[i].PlayerStats.isDead)
+                {
+                    gc.players[id].PlayerCamera.enabled = false;
+                    gc.players[i].PlayerCamera.enabled = true;
+                }
+            }
+        }
+    }
+}
