7f349c5abc2352de59836ecb3b1dab1600e81035
Pim <pim103@hotmail.fr>
2019-07-12 14:49:56 +0200

    Add LookAt to id soundBlock

--- Unity/Assets/GarbageRoyale/Scripts/PlayerController/SoundBlockController.cs
+++ Unity/Assets/GarbageRoyale/Scripts/PlayerController/SoundBlockController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Photon.Pun;
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -6,15 +7,31 @@ namespace GarbageRoyale.Scripts.PlayerController
 {
     public class SoundBlockController : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject textContainer;
+
+        [SerializeField]
+        private TextMesh textIdPlayer;
+
+        private GameController gc;
+
         public int id;
         public int playerId;
+        public int actualIdPlayer;
 
         public bool hasBeenSeenByMob = false;
-        
+
+        private void Start()
+        {
+            gc = GameObject.Find("Controller").GetComponent<GameController>();
+            actualIdPlayer = Array.IndexOf(gc.AvatarToUserId, PhotonNetwork.AuthValues.UserId);
+        }
+
         private void OnEnable()
         {
             StartCoroutine(DeactivateMyself());
-            transform.GetChild(0).GetComponent<TextMesh>().text = playerId.ToString();
+            StartCoroutine(LookAtPlayer());
+            textIdPlayer.text = playerId.ToString();
         }
 
         IEnumerator DeactivateMyself()
@@ -23,6 +40,15 @@ namespace GarbageRoyale.Scripts.PlayerController
             {
                 yield return new WaitForSeconds(29.0f);
                 gameObject.SetActive(false);
+            }
+        }
+
+        IEnumerator LookAtPlayer()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(0.1f);
+                textContainer.transform.LookAt(gc.players[actualIdPlayer].PlayerCamera.transform);
             }
         }
     }
